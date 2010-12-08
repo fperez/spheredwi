e0d24e1f79b15792f815ce967c75fc588e478857
Fernando Perez <Fernando.Perez@berkeley.edu>
2010-12-08 13:10:08 -0800

    Structure code as python package, add tests.

--- src/python/sphdif/tests/test_sphquad.py
+++ src/python/sphdif/tests/test_sphquad.py
@@ -6,6 +6,7 @@
 from __future__ import print_function
 
 # Stdlib imports
+import os
 
 # Third-party imports
 import nose.tools as nt
@@ -13,7 +14,7 @@ import numpy as np
 import numpy.testing as npt
 
 # Our own imports
-import sphquad
+from .. import sphquad
 
 #-----------------------------------------------------------------------------
 # Classes and functions
@@ -22,3 +23,10 @@ import sphquad
 def test_legp():
     p = sphquad.legp(0, 0)
     npt.assert_equal(p[0], 1)
+    #path = os.path.join(
+    my_dir = os.path.split(__file__)[0]
+    legp_true = np.loadtxt(os.path.join(my_dir, 'legendrep.txt'))
+    max_order = legp_true.shape[0] - 1
+    x_grid = legp_true[1]
+    legp = sphquad.legp(x_grid, max_order)
+    npt.assert_almost_equal(legp, legp_true, 13)
