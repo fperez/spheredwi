b64c95a2eab9fc7b4bd797907d4db7d608109b8b
Stefan van der Walt <stefan@sun.ac.za>
2012-02-23 15:15:04 -0800

    Clean up kernel_peak_detection.

--- src/python/kernel_peak_detection.py
+++ src/python/kernel_peak_detection.py
@@ -17,7 +17,6 @@ from signal_sim import single_tensor, single_tensor_ODF
 # Experiment configuration
 # ========================
 
-Q = 72  # Number of quadrature points
 gamma = np.deg2rad(10)  # Angle separating fibers
 D = 150 # Grid density for plots (higher => more dense)
 
@@ -25,7 +24,9 @@ D = 150 # Grid density for plots (higher => more dense)
 # Load quadrature and set up test data
 # ====================================
 
-theta, phi, w = sphere.quadrature_points(N=Q)
+theta72, phi72, w72 = sphere.quadrature_points(N=72)
+theta132, phi132, w132 = sphere.quadrature_points(N=132)
+# theta492, phi492, w492 = sphere.quadrature_points(N=492)
 
 # b is tau * |q|^2 in s/mm^2
 # If b is too low, the signal does not attenuate enough to measure.
@@ -34,89 +35,25 @@ theta, phi, w = sphere.quadrature_points(N=Q)
 # Make up somewhat realistic b-values
 b = 995 + np.random.normal(scale=4, size=Q)
 
-xyz = np.column_stack(sphere.sph2car(np.ones_like(theta), theta, phi))
-
 R0 = rotation_around_axis([0, 1, 0], gamma / 2.)
 R1 = rotation_around_axis([0, 1, 0], -gamma / 2.)
+
+xyz = np.column_stack(sphere.sph2car(np.ones_like(theta), theta, phi))
 
 S = single_tensor(gradients=xyz, bvals=b, S0=1, rotation=R0, SNR=None)
 S += single_tensor(gradients=xyz, bvals=b, S0=1, rotation=R1, SNR=None)
 
-# Visualise ODF
-lat = np.linspace(-90, 90, D)
-lon = np.linspace(-180, 180, D)
-theta_grid = np.linspace(0, np.pi, D)
-phi_grid = np.linspace(0, 2 * np.pi, D)
+# ================
+# Q-Space Sampling
+# ================
 
+# ===========================-=====
+# ODF-domain: Sparse reconstruction
+# =================================
 
-mg_phi, mg_theta = np.meshgrid(phi_grid, theta_grid)
-xyz = np.dstack(sphere.sph2car(np.ones(mg_theta.shape), mg_theta, mg_phi))
-
-## ODF
-#ODF = single_tensor_ODF(xyz.reshape((-1, 3)), rotation=None)
-
-# Q-space signal
-ODF = single_tensor(gradients=xyz.reshape((-1, 3)), bvals=1000*np.ones(D*D),
-                    S0=1, rotation=R0, SNR=None)
-
-ODF = ODF.reshape(mg_theta.shape)
-
-xyz = np.dstack(sphere.sph2car(ODF, mg_theta, mg_phi))
-
-def basemap_ODF():
-    m = sphere.surf_grid(ODF,
-                         theta_grid, phi_grid,
-                         vmin=0, vmax=1, projection='moll')
-    plt.show()
-
-def mayavi_ODF():
-    from mayavi import mlab
-    x, y, z = np.rollaxis(xyz, -1)
-    mlab.mesh(x, y, z, scalars=ODF)
-    #mlab.plot3d(x.ravel(), y.ravel(), z.ravel(), representation='points')
-    mlab.show()
-
-mayavi_ODF()
-
-## def peak(theta, phi, sigma, theta_all, phi_all):
-##     """Generate a peak of width sigma at (theta, phi)
-##     with theta the polar angle, phi the azimuthal angle.
-##     """
-
-##     p = np.exp(-sphere.arc_length(theta, phi, theta_all, phi_all)**2 / float(sigma**2))
-##     p /= p.max()
-
-##     return p
-
-
-
-## # Load quadrature points
-## q_theta_72, q_phi_72, q_w_72 = sphere.quadrature_points(72)
-## q_theta_132, q_phi_132, q_w_132 = sphere.quadrature_points(132)
-## q_theta_492, q_phi_492, q_w_492 = sphere.quadrature_points(492)
-
-# PDF parameters
-## pdf_theta = [1.8 * np.pi / 3, np.pi / 3]
-## pdf_phi = [2.5 * np.pi / 3, np.pi]
-## pdf_sigma = [0.5, 0.5]
-
-## def PDF(theta, phi):
-##     """Evaluate the simulated PDF on the given points.
-
-##     """
-##     M, N = len(theta), len(phi)
-##     pdf = np.zeros((M, N))
-
-##     for (p_theta, p_phi, p_sigma) in zip(pdf_theta, pdf_phi, pdf_sigma):
-##         pdf += peak(p_theta, p_phi, p_sigma, theta, phi)
-
-##     return pdf / pdf.max()
-
-
-## # <markdowncell>
-
-## # # Test signal construction
-
+# ==========================
+# ODF-domain: Peak detection
+# ==========================
 
 
 ## # Sample signal at several points
@@ -125,14 +62,6 @@ mayavi_ODF()
 ## for (theta, phi, sigma) in zip(pdf_theta, pdf_phi, pdf_sigma):
 ##     samples += peak(theta, phi, sigma, q_theta_72, q_phi_72)
 
-## # <markdowncell>
-
-## # # Kernels
-
-
-
-
-## # <markdowncell>
 
 ## # # Fitting
 
@@ -250,13 +179,3 @@ mayavi_ODF()
 
 ## print "Number of coefficients: %d" % np.sum(beta != 0)
 
-## # <markdowncell>
-
-## # <div
-## # style="border-top: solid 5px;"><br/></span>
-## #
-## # # Experimental
-
-
-
-## # Use Mayavi to surf kernel surface
