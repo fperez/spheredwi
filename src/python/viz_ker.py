809f9c22e7272709de530888c1e81f24b44affd2
Stefan van der Walt <stefan@sun.ac.za>
2012-02-24 15:54:47 -0800

    Improve sphdif load time by lazily loading mlab.

--- src/python/viz_ker.py
+++ src/python/viz_ker.py
@@ -1,8 +1,10 @@
 """Visualize inverse Funk-Radon transform elements with mayavi.
 """
 from sphdif.kernel import kernel_plot, inv_funk_radon_even_kernel
-from sphdif.plot import show, mlab
+from sphdif.plot import get_mlab
+
+mlab = get_mlab()
 
 mlab.figure(bgcolor=(1, 1, 1), fgcolor=(0, 0, 0))
 kernel_plot(inv_funk_radon_even_kernel, N=14)
-show()
+mlab.show()
