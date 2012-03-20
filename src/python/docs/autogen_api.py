d9cf28be5c21a8032f22b14cc9197726bd2b4e9b
Stefan van der Walt <stefan@sun.ac.za>
2012-03-20 15:57:51 -0700

    Add Sphinx documentation infrastructure.

---
+++ src/python/docs/autogen_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""Script to auto-generate our API docs.
+"""
+# stdlib imports
+import os
+import sys
+
+# local imports
+sys.path.append(os.path.abspath('sphinxext'))
+sys.path.insert(0, os.path.abspath('../'))
+from apigen import ApiDocWriter
+
+#*****************************************************************************
+if __name__ == '__main__':
+    pjoin = os.path.join
+    package = 'sphdif'
+    outdir = pjoin('source', 'api', 'generated')
+    docwriter = ApiDocWriter(package, rst_extension='.txt')
+    # You have to escape the . here because . is a special char for regexps.
+    # You must do make clean if you change this!
+#    docwriter.package_skip_patterns += []
+
+    docwriter.module_skip_patterns += [ r'\.minroutines']
+
+    # Now, generate the outputs
+    docwriter.write_api_docs(outdir)
+    docwriter.write_index(outdir, 'gen',
+                          relative_to = pjoin('source','api')
+                          )
+    print '%d files written' % len(docwriter.written_modules)
