fdc10c4ef6cb373896c4251b495bd04b7742d7d0
Stefan van der Walt <stefan@sun.ac.za>
2012-03-22 16:32:38 -0700

    Small refactoring to io.  Add remove.

--- src/python/sphdif/sph_io.py
+++ src/python/sphdif/sph_io.py
@@ -6,9 +6,10 @@ import os
 data_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../tmp')
 
 def temp_storage(f):
-    def first_check_temp_dir(*args, **kwargs):
+    def first_check_temp_dir(file, *args, **kwargs):
         if not os.path.isdir(data_path):
             os.mkdir(data_path)
+        args = (os.path.join(data_path, file),) + args[1:]
         return f(*args, **kwargs)
 
     first_check_temp_dir.__doc__ = f.__doc__
@@ -19,15 +20,26 @@ def save(file, arr):
     """Proxy to NumPy's save command that stores to a temporary directory.
 
     """
+    return np.save(file, arr)
 
-    f = os.path.join(data_path, file)
-    return np.save(f, arr)
+@temp_storage
+def savez(file, *args, **kwargs):
+    """Proxy to NumPy's savez command that stores to a temporary directory.
+
+    """
+    return np.savez(file, *args, **kwargs)
 
 @temp_storage
 def load(file):
     """Proxy to NumPy's load command that reads from a temporary directory.
 
     """
+    return np.load(file)
 
-    f = os.path.join(data_path, file)
-    return np.load(f)
+@temp_storage
+def remove(file):
+    """Proxy to NumPy's savez command that stores to a temporary directory.
+
+    """
+    import os
+    os.unlink(file)
